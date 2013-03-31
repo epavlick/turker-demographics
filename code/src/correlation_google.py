6339e4f3801afa5596d5b32364f3c59093aba25e
steven <steven@steven-NBLBX.(none)>
2013-03-31 10:48:58 -0400

    small changes

--- code/src/correlation_google.py
+++ code/src/correlation_google.py
@@ -13,12 +13,12 @@ file2 = sys.argv[2]
 
 scores1 = list(); scores2 = list()
 
-for s1, s2 in zip(open(file1).readlines(), open(file2).readlines()):
-	s1 = s1.strip(); s2 = s2.strip();
-	if s1 == 'N/A' or s2 == 'N/A':
+for s1, s2 in zip(open(file1).readlines()[1:], open(file2).readlines()[1:]):
+	s1 = s1.strip().split(); s2 = s2.strip().split();
+	if s1[3] == 'N/A' or s2[3] == 'N/A':
 		continue
-	scores1.append(float(s1))
-	scores2.append(float(s2))
+	scores1.append(float(s1[3]))
+	scores2.append(float(s2[3]))
 
 
 print 'Their average:', sum(scores1)/len(scores1)
